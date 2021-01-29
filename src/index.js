cab701a12d91b726469f49289737a1e9e8ccb025
Joma121 <martinsjoshua1986@gmail.com>
2021-01-28 18:07:59 -0800

    adds fetch for berry and joke, and renders components berry, joke and button

--- src/index.js
+++ src/index.js
@@ -1,34 +1,55 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import Section from "./Section";
+import Joke from "./Joke";
+import Berry from "./Berry";
+import Button from "./Button";
+import {Cat} from 'react-kawaii';
 
 import "./styles.css";
 
-const apiKey = process.env.REACT_APP_API_KEY;
-const searchTerm = "art";
-const fetchUrl = `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&limit=50&api_key=${apiKey}`;
+const fetchJoke = `https://official-joke-api.appspot.com/jokes/programming/random`;
+const fetchBerries =`https://pokeapi.co/api/v2/berry/`;
+
 
 class ArtApi extends Component {
 	state = {
-		apiData: [],
+		berryData: [],
+		jokeData: [],
+		mood: "sad",
 	};
 
 	componentDidMount() {
-		fetch(fetchUrl)
+		this.goFetch();
+	};
+
+	goFetch = () => {
+		fetch(fetchBerries)
 			.then((res) => res.json())
-			.then((json) => this.setState({ apiData: json.data }));
+			.then((json) => this.setState({ berryData: json.results }));
+		fetch(fetchJoke)
+			.then((res) => res.json())
+			.then((json) => this.setState({ jokeData: json }));
+	};
+
+	componentDidUpdate() {
+		if(this.state.berryData && this.state.jokeData && this.state.mood !== "happy"){
+			return this.setState({mood: "happy"});
+		}
+		return "sad";
 	}
 
 	render() {
+		console.log(this.state.berryData)
+		console.log(this.state.jokeData)
 		return (
 			<div className="main">
-				<Section apiData={this.state.apiData} />
-				<Section apiData={this.state.apiData} />
-				<Section apiData={this.state.apiData} />
-				<Section apiData={this.state.apiData} />
+				<Cat size={320} mood={this.state.mood} color="#596881" />
+				<Berry apiData={this.state.berryData} />
+				<Joke apiData={this.state.jokeData} />
+				<Button newFetch={this.goFetch}/>
 			</div>
 		);
-	}
+	};
 }
 
 export default ArtApi;
